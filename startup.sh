1f7d770366cd5ea6228007d1829a696e43a97b94
Anubhav2912 <ak23.ideal@gmail.com>
2024-04-30 15:11:34 +0530

    updated startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 myproject.wsgi
-Bash
-Create a .gitignore file in the django-apprunner/myproject directory:
-
-*.log
-*.pot
-*.pyc
-__pycache__
-db.sqlite3
-media
-staticfiles
+python manage.py collectstatic && gunicorn --workers 2 bloodbankmanagement.wsgi
